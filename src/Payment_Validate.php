46e1135845c3740d9d207826389487d87a63a5a7
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-05-25 00:27:29 +0530

    Unreserved Ticket source code updated

---
+++ src/Payment_Validate.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+require('Payment_Manager.php');
+
+$name = $_POST['cname'];
+$cno = $_POST['cno'];
+$ed = $_POST['ed'];
+$cvv = $_POST['cvv'];
+$fare = $_SESSION['fare'];
+
+$ed = (string)$ed;
+$fare = (double)$fare;
+$ob = new Payment_Manager();
+$val = $ob->validate($name,$cno,$ed,$cvv,$fare);
+
+if($val)
+{
+    header('location:print_uticket.php');
+}
+else
+{
+    echo '<script>alert("Payment Failure")
+    window.location = "Unreserved_Ticket.html"
+    </script>';
+}
+
+?>
