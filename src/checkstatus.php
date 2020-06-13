c823c1215bb4ed3214e6f800e6e52d6cdc5b4594
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-06-13 11:31:46 +0530

    update check_status.php

--- src/checkstatus.php
+++ src/checkstatus.php
@@ -7,28 +7,37 @@ $id = $_POST['id1'];
 $id = (int)$id;
 
 $ob = new adminfunc();
-$val = $ob->validate($id);
-if($val)
+$exist = $ob->existId($id);
+if(!$exist)
 {
-    $_SESSION['id'] = $id;
-
-    $conn = new mysqli('localhost','root');
-    mysqli_select_db($conn,'tram');
-    $stmt = "select * from special_ticket where id='$id'";
-    $result = mysqli_query($conn,$stmt);
-    $num = mysqli_num_rows($result);
-    if($num>0)
-    {
-        header('location:display_spass.php');
-    }
-    else
-    {
-        echo '<script>alert("Booking Confirmed")
-        window.location = "Spayment_Gateway.php"</script>';
-    }
+    echo '<script>alert("Flushed Booking ID")
+    window.location = "check_status.html"</script>';
 }
 else
 {
-    echo "Booking not Confirmed";
+    $val = $ob->validate($id);
+    if($val)
+    {
+        $_SESSION['id'] = $id;
+
+        $conn = new mysqli('localhost','root');
+        mysqli_select_db($conn,'tram');
+        $stmt = "select * from special_ticket where id='$id'";
+        $result = mysqli_query($conn,$stmt);
+        $num = mysqli_num_rows($result);
+        if($num>0)
+        {
+            header('location:display_spass.php');
+        }
+        else
+        {
+            echo '<script>alert("Booking Confirmed")
+            window.location = "Spayment_Gateway.php"</script>';
+        }
+    }
+    else
+    {
+        echo "Booking not Confirmed";
+    }
 }
 ?>
