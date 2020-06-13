acad64ce7752b11ad25160b3b1dfb564c33dc61a
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-06-13 11:33:46 +0530

    Updated data.php

--- src/data.php
+++ src/data.php
@@ -1,23 +1,50 @@
 <?php
 
 session_start();
-
-$conn = new mysqli('localhost','root');
-if(!$conn)
-{
-    echo "Fail";
-}
-
-mysqli_select_db($conn, 'tram');
+require('Location_Manager.php');
 
 $user = $_POST['user'];
 $lat = $_POST['lat'];
 $long = $_POST['lon'];
 
-$stmt = "insert into live_location(tramid,latitude,longitude) values('$user','$lat','$long')";
-mysqli_query($conn, $stmt);
-$_SESSION['user']=$user;
-echo $user;
-header('location:response.php');
+
+$ob = new Location_Manager();
+
+$check = $ob->validId($user);
+
+if(!$check)
+{
+    echo'<script>alert("No such tramid exist")
+    window.location="server.html"</script>';
+}
+else
+{
+    $val = $ob->insert($user,$lat,$long);
+    /*$conn = new mysqli('localhost','root');
+    if(!$conn)
+    {
+        echo "Fail";
+    }
+
+    mysqli_select_db($conn, 'tram');
+
+    $user = $_POST['user'];
+    $lat = $_POST['lat'];
+    $long = $_POST['lon'];
+
+    $stmt = "insert into live_location(tramid,latitude,longitude) values('$user','$lat','$long')";
+    mysqli_query($conn, $stmt);
+    $_SESSION['user']=$user;
+    echo $user;*/
+    if($val)
+    {
+        $_SESSION['user']=$user;
+        header('location:response.php');
+    }
+    else
+    {
+        echo "Link Failure";
+    }
+}
 
 ?>
