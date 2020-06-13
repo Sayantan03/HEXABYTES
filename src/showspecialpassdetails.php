dfac33efc9a8a562fb44c1993380d37b1cef48dc
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-06-13 13:05:59 +0530

    Update showspecialpassdetails.php
    Additional features in 1.0 added

--- src/showspecialpassdetails.php
+++ src/showspecialpassdetails.php
@@ -42,15 +42,28 @@ $result = mysqli_query($conn,$stmt);
                 text-align: center;
                 font-size : 20px;
             }
+            .button
+            {
+                background-color: blue;
+                color: whitesmoke;
+                padding: 15px 32px;
+                text-align: center;
+                display: inline-block;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 15px;
+            }
         </style>
     </head>
     <body>
         <h1><u>Special Booking Details</u></h1><span id="r"><a href="admin_control.html">Go Back to Home Page</a></span>
         <?php
+            $sum = 0.00;
             echo "<table>";
             echo "<tr><th>Ticket Id</th><th>Passenger Name</th><th>Booking Purpose</th><th>Booking Date</th><th>Start Time</th><th>End Time</th><th>Fare(Rs.)</th>";
             while($row = mysqli_fetch_array($result))
             {
+                $sum = $sum + (double)$row['fare'];
                 echo "<tr>";
                 echo "<td>".$row['id']."</td>";
                 echo "<td>".$row['cname']."</td>";
@@ -63,5 +76,15 @@ $result = mysqli_query($conn,$stmt);
             }
             echo "</table>";
         ?>
+        <br><button class="button" type="button" onclick="showtotal()">Click to get total fare till date</button><br>
+        <h4><span id="sh"></span><span id="s"></span></h4>
+        <script>
+            function showtotal()
+            {
+                //alert("OK");
+                document.getElementById('sh').innerHTML="RS::";
+                document.getElementById('s').innerHTML="<?php echo "$sum";?>.00";
+            }
+        </script>
     </body>
 </html>
