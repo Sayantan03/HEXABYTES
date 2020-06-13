c954a2b8f6cfb583a467a47f9d7640f8e5c7292c
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-06-13 13:11:36 +0530

    updated showunreservedticketdetails.php
    1.0 extra features added

--- src/showunreservedticketdetails.php
+++ src/showunreservedticketdetails.php
@@ -28,7 +28,7 @@ $result = mysqli_query($conn,$stmt);
             }
             table
             {
-                margin-top: 8%;
+                margin-top: 5%;
                 margin-left: 35%;
                 padding: 40px;
                 border: 1px solid;
@@ -42,15 +42,28 @@ $result = mysqli_query($conn,$stmt);
                 text-align: center;
                 font-size : 20px;
             }
+            .button
+            {
+                background-color: blue;
+                color: whitesmoke;
+                padding: 15px 32px;
+                text-align: center;
+                display: inline-block;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 15px;
+            }
         </style>
     </head>
     <body>
         <h1><u>Unreserved Ticket Details</u></h1><span id="r"><a href="admin_control.html">Go Back to Home Page</a></span>
         <?php
+            $sum = 0.00;
             echo "<table>";
             echo "<tr><th>Ticket Id</th><th>Ticket Sold Date</th><th>Fare(Rs.)</th>";
             while($row = mysqli_fetch_array($result))
             {
+                $sum = $sum + (double)$row['fare'];
                 echo "<tr>";
                 echo "<td>".$row['id']."</td>";
                 echo "<td>".$row['date']."</td>";
@@ -59,5 +72,15 @@ $result = mysqli_query($conn,$stmt);
             }
             echo "</table>";
         ?>
+        <br><button class="button" type="button" onclick="showtotal()">Click to get total fare till date</button><br>
+        <h4><span id="sh"></span><span id="s"></span></h4>
+        <script>
+            function showtotal()
+            {
+                //alert("OK");
+                document.getElementById('sh').innerHTML="RS::";
+                document.getElementById('s').innerHTML="<?php echo "$sum";?>.00";
+            }
+        </script>
     </body>
 </html>
